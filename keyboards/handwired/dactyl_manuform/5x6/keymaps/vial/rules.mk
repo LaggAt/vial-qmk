8e248713461cae99eb9d8d20cad82ec903b7b3d6
Florian Lagg <florian.lagg@gmail.com>
2023-10-29 11:50:09 +0100

    basic vial config, missing my rotary encoder and joystick

---
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/vial/rules.mk
@@ -0,0 +1,11 @@
+# enable change of keyboard layout using https://vial.rocks/ or download application from https://get.vial.today/
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+# for now, this.
+# TODO: https://get.vial.today/docs/porting-to-vial.html#6-set-up-a-secure-unlock-combination
+VIAL_INSECURE = yes
+
+# shrink firmware
+LTO_ENABLE = yes
+QMK_SETTINGS = no
